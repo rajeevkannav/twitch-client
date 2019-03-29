9442ed8550571b22d781ff9f5bc5f752505254cd
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-29 17:02:20 +0530

    Updates

---
+++ precache-manifest.fdfc1481e12798bf71d69b10864c688e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdfc7d3a8e2ed38b12c2",
+    "url": "/twitch-client/static/js/runtime~main.2866d897.js"
+  },
+  {
+    "revision": "0e98e1dbedc5a959712f",
+    "url": "/twitch-client/static/js/main.d36cd229.chunk.js"
+  },
+  {
+    "revision": "81bf15e7deafd84fd9a7",
+    "url": "/twitch-client/static/js/2.bb377a9f.chunk.js"
+  },
+  {
+    "revision": "5d6b09f6365223dcad7f54ba553c25f7",
+    "url": "/twitch-client/index.html"
+  }
+];
