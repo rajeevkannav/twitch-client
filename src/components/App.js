6e1c7342cb8a2f4513d9045742f266dd77436190
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-29 16:58:26 +0530

    added Google Oauth2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+//Application
+import StreamCreate from './streams/Create';
+import StreamEdit from './streams/Edit';
+import StreamShow from './streams/Show';
+import StreamDelete from './streams/Delete';
+import StreamList from './streams/List';
+
+import Header from './shared/Header';
 
 const App = () => {
-    return <div>Hello there!!!</div>
-}
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <Header />
+                <div>
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' exact component={StreamCreate}/>
+                    <Route path='/streams/edit' exact component={StreamEdit}/>
+                    <Route path='/streams/delete' exact component={StreamDelete}/>
+                    <Route path='/streams/show' exact component={StreamShow}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+};
 
 export default App;
