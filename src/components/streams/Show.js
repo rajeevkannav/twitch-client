9bf11db8030174545ecaac405126c4138733fcb0
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-04-08 12:22:17 +0530

    stream app completed

--- src/components/streams/Show.js
+++ src/components/streams/Show.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from "../../actions";
 
 class Show extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        const {id} = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer()
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+        const {id} = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.buildPlayer()
+    }
+
+    componentWillUnmount() {
+        this.player.destroy()
     }
 
     render() {
@@ -15,6 +45,10 @@ class Show extends React.Component {
         const {title, description} = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef}
+                       style={{width: '100%'}}
+                       controls
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
